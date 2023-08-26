e4029486709f1dfd3b24447c940adcb5ff1b6834
gustavocortesdata <gcortes@utryt.com.co>
2023-08-25 20:48:38 -0500

    feat: Nueva APP

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+DEBUG=True
+SECRET_KEY='3E803AD67A2C952F'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:UgRH5522@localhost:5432/postgres'
+SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+#basedir = os.path.abspath(os.path.dirname(__file__))
